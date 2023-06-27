21517861bc223c10283bd34065f60efcb8e6cb54
SOHAM <87080389+Soham-0047@users.noreply.github.com>
2023-06-27 07:51:44 +0530

    Added solution - LeetHub

---
+++ Insertion Sort - GFG/insertion-sort.cpp
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+// C program for insertion sort
+#include <stdio.h>
+#include <math.h>
+
+/* Function to print an array */
+void printArray(int arr[], int size)
+{
+    int i;
+    for (i=0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    void insert(int arr[], int i)
+    {
+        //code here
+    }
+     public:
+    //Function to sort the array using insertion sort algorithm.
+    void insertionSort(int arr[], int n)
+    {
+        for(int i=0;i<n;i++){
+        int l =i; //Store the first index of next iteration
+        while(l>0 && arr[l-1] > arr[l]){
+            int tmp = arr[l-1];  //Swap the smaller and larger values
+            arr[l-1] = arr[l];
+            arr[l] = tmp;
+            l--; //restore the previous value of l
+        }
+    }
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int arr[1000],n,T,i;
+
+    scanf("%d",&T);
+
+    while(T--){
+
+    scanf("%d",&n);
+
+    for(i=0;i<n;i++)
+      scanf("%d",&arr[i]);
+
+    Solution ob;
+    ob.insertionSort(arr, n);
+    printArray(arr, n);
+    }
+    return 0;
+}
+
+// } Driver Code Ends
